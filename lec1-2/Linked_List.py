2f1deeaa574378b681f00527f906ab70ff870cdb
dongxubin87 <dongxubin87@gmail.com>
2025-07-23 21:21:17 +0300

    23-7

--- lec1-2/Linked_List.py
+++ lec1-2/Linked_List.py
@@ -32,6 +32,10 @@ class Linked_List_Seq:
         node = self.head.later_node(i)
         return node.item
 
+    def set_at(self, i, x):
+        node = self.head.later_node(i)
+        node.item = x
+
     def insert_first(self, x):
         node = Linked_List_Node(x)
         node.next = self.head
@@ -96,3 +100,114 @@ loop_start_node = lst.head.later_node(0)
 last_node.next = loop_start_node
 length = find_cycle(lst)
 print("length", length)
+
+
+def Set_from_Seq(seq):
+    class set_from_seq:
+        def __init__(self):
+            self.S = seq()
+
+        def __len__(self):
+            return len(self.S)
+
+        def __iter__(self):
+            yield from self.S
+
+        def insert(self, x):
+            for i in range(len(self.S)):
+                if self.S.get_at(i).key == x.key:
+                    self.S.set_at(i, x)
+                    return
+            self.S.insert_last(x)
+
+        def delete(self, x):
+            for i in range(len(self.S)):
+                if self.S.get_at(i).key == x:
+                    return self.S.delete_at(i)
+
+        def find(self, x):
+            for e in self:
+                if e.key == x:
+                    return e
+            return None
+
+        def find_min(self):
+            min = None
+            for x in self:
+                if min is None or x.key < min.key:
+                    min = x
+            return min
+
+        def find_max(self):
+            max = None
+            for x in self:
+                if max is None or x.key > max.key:
+                    max = x
+            return max
+
+        def find_next(self, x):
+            res = None
+            for e in self:
+                if e.key > x:
+                    if res is None or e.key < res.key:
+                        res = e
+            return res
+
+        def find_prev(self, x):
+            res = None
+            for e in self:
+                if e < x:
+                    if res is None or res.key > e.key:
+                        res = e
+            return res
+
+        def iter_ord(self):
+            x = self.find_min()
+            while x:
+                yield x
+                x = self.find_next(x.key)
+
+    return set_from_seq
+
+
+"""
+Jen drives her ice cream truck to her local elementary school at recess. All the kids rush to line up
+in front of her truck. Jen is overwhelmed with the number of students (there are 2n of them), so
+she calls up her associate, Berry, to bring his ice cream truck to help her out. Berry soon arrives
+and parks at the other end of the line of students. He offers to sell to the last student in line, but the
+other students revolt in protest: “The last student was last! This is unfair!”
+The students decide that the fairest way to remedy the situation would be to have the back half of
+the line (the n kids furthest from Jen) reverse their order and queue up at Berry’s truck, so that the
+last kid in the original line becomes the last kid in Berry’s line, with the (n+1)st kid in the original
+line becoming Berry’s first customer.
+(a) Given a linked list containing the names of the 2n kids, in order of the original line
+formed in front of Jen’s truck (where the first node contains the name of the first kid
+in line), describe an O(n)-time algorithm to modify the linked list to reverse the order
+of the last half of the list. Your algorithm should not make any new linked list nodes
+or instantiate any new non-constant-sized data structures during its operation. """
+
+
+def reverse_linklist(L):
+    n = len(L) // 2
+    a = L.head
+    for _ in range(n - 1):
+        a = a.next
+    # a is n item
+    curr = a.next
+    prev = None
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    a.next = prev
+
+
+L = Linked_List_Seq()
+L.build([1, 2, 3, 4, 5, 6])
+for x in L:
+    print(x)
+
+reverse_linklist(L)
+for x in L:
+    print(x)
