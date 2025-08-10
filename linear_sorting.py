71555d67ea53d23dcbeb1a3c9f4e75f57c63d89a
dongxubin87 <dongxubin87@gmail.com>
2025-08-10 23:37:39 +0300

    lec5

--- linear_sorting.py
+++ linear_sorting.py
@@ -23,17 +23,41 @@ def counting_sortA(A):
 
 
 def counting_sortB(A):
-    u = 1 + max(A)
+    u = 1 + max([x.key for x in A])
     D = [0] * u
     for x in A:
-        D[x] += 1
+        D[x.key] += 1
     for k in range(1, u):
         D[k] += D[k - 1]
     for x in list(reversed(A)):
-        A[D[x] - 1] = x
-        D[x] -= 1
+        A[D[x.key] - 1] = x
+        D[x.key] -= 1
 
 
-A = [4, 4, 6, 3, 6, 5, 1, 2, 3]
-counting_sortB(A)
+def radix_sort(A):
+    n = len(A)
+    u = max(A)
+    c = 1 + u.bit_length() // n.bit_length()
+
+    class Obj:
+        pass
+
+    D = [Obj() for a in A]
+    for i in range(n):
+        D[i].digits = []
+        D[i].item = A[i]
+        high = A[i]
+        for j in range(c):
+            high, low = divmod(high, 10)
+            D[i].digits.append(low)
+    for i in range(c):
+        for j in range(n):
+            D[j].key = D[j].digits[i]
+        counting_sortB(D)
+    for i in range(n):
+        A[i] = D[i].item
+
+
+A = [329, 457, 657, 999, 839, 436, 720, 355]
+radix_sort(A)
 print(A)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+  n = len(A)
+    u = 1 + max(A)
+    c = 1 + u.bit_length() // n.bit_length()
